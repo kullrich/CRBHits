42fc910710041096461a2fba0e60c0c6eec56273
Kristian Ullrich <ullrich@evolbio.mpg.de>
2020-10-28 19:45:59 +0100

    #update test functions

--- tests/testthat/test-cdsfile2rbh.R
+++ tests/testthat/test-cdsfile2rbh.R
@@ -1,12 +1,12 @@
 test_that("cdsfile2rbh() outputs correct number of conditional reciprocal best hit pairs", {
   #CRBHits_root <- system.file(package = "CRBHits")
   #LastTempDir <- tempdir()
-  #system(paste0("unzip -o ", CRBHits_root, "/extdata/last-1080.zip -d ", LastTempDir))
-  #system(paste0("cd ", LastTempDir, "/last-1080/; make"))
+  #system(paste0("unzip -o ", CRBHits_root, "/extdata/last-1133.zip -d ", LastTempDir))
+  #system(paste0("cd ", LastTempDir, "/last-1133/; make"))
   tmp.paths <- make.vignette()
   athfile <- system.file("fasta/ath.cds.fasta.gz", package = "CRBHits")
   alyfile <- system.file("fasta/aly.cds.fasta.gz", package = "CRBHits")
-  #ath_aly_crbh <- cdsfile2rbh(athfile, alyfile, lastpath = paste0(LastTempDir, "/last-1080/src/"))
+  #ath_aly_crbh <- cdsfile2rbh(athfile, alyfile, lastpath = paste0(LastTempDir, "/last-1133/src/"))
 
   ath_aly_crbh <- cdsfile2rbh(athfile, alyfile, lastpath = tmp.paths[1])
   expect_equal(dim(ath_aly_crbh$crbh.pairs)[1], 798)
