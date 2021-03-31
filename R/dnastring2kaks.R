56df627c17654dd3a886a3053435f2733b82c172
Kristian Ullrich <ullrich@evolbio.mpg.de>
2021-03-31 09:57:04 +0200

    added melt to directly get ka/ks matrix for plotting

--- R/dnastring2kaks.R
+++ R/dnastring2kaks.R
@@ -11,6 +11,8 @@
 #' @importFrom seqinr kaks
 #' @importFrom doMC registerDoMC
 #' @importFrom foreach foreach %do% %dopar%
+#' @importFrom tidyr %>% as_tibble pivot_longer
+#' @importFrom dplyr slice left_join
 #' @seealso \code{\link[seqinr]{kaks}} \code{\link[CRBHits]{cds2codonaln}} \code{\link[Biostrings]{pairwiseAlignment}}
 #' @references Nei and Gojobori. (1986) Simple methods for estimating the numbers of synonymous and nonsynonymous nucleotide substitutions. \emph{Mol. Biol. Evol.}, \bold{3(5)}, 418-426.
 #' @references Ganeshan et al. (1997) Human immunodeficiency virus type 1 genetic evolution in children with different rates of development of disease. \emph{J. Virology.} \bold{71(1)}, 663-677.
@@ -37,7 +39,24 @@ dnastring2kaks <- function(cds,
                            ...){
   if(!model %in% c("Li", "NG86")){stop("Error: either choose model 'Li' or 'NG86'")}
   if(model == "Li" & align == FALSE){
-    return(seqinr::kaks(dnastring2aln(cds)))
+    OUT <- seqinr::kaks(dnastring2aln(cds))
+    OUT.ka <- as.matrix(OUT$ka)
+    colnames(OUT.ka) <- rownames(OUT.ka) <- gsub(" ", "", colnames(OUT.ka))
+    OUT.ks <- as.matrix(OUT$ks)
+    colnames(OUT.ks) <- rownames(OUT.ks) <- gsub(" ", "", colnames(OUT.ks))
+    OUT.vka <- as.matrix(OUT$vka)
+    colnames(OUT.vka) <- rownames(OUT.vka) <- gsub(" ", "", colnames(OUT.vka))
+    OUT.vks <- as.matrix(OUT$vks)
+    colnames(OUT.vks) <- rownames(OUT.vks) <- gsub(" ", "", colnames(OUT.vks))
+    OUT.ka <- OUT.ka %>% tidyr::as_tibble(rownames = "seq1") %>% tidyr::pivot_longer(colnames(OUT.ka), names_to = "seq2", values_to = "ka") %>% dplyr::slice(uptriidx(ncol(OUT.ka)))
+    OUT.ks <- OUT.ks %>% tidyr::as_tibble(rownames = "seq1") %>% tidyr::pivot_longer(colnames(OUT.ks), names_to = "seq2", values_to = "ks") %>% dplyr::slice(uptriidx(ncol(OUT.ks)))
+    OUT.vka <- OUT.vka %>% tidyr::as_tibble(rownames = "seq1") %>% tidyr::pivot_longer(colnames(OUT.vka), names_to = "seq2", values_to = "vka") %>% dplyr::slice(uptriidx(ncol(OUT.vka)))
+    OUT.vks <- OUT.vks %>% tidyr::as_tibble(rownames = "seq1") %>% tidyr::pivot_longer(colnames(OUT.vks), names_to = "seq2", values_to = "vks") %>% dplyr::slice(uptriidx(ncol(OUT.vks)))
+    OUT <- OUT.ka %>% dplyr::left_join(OUT.ks) %>% dplyr::left_join(OUT.vka) %>% dplyr::left_join(OUT.vks)
+    OUT <- as.data.frame(OUT)
+    attr(OUT, "model") <- "Li"
+    attr(OUT, "align") <- "FALSE"
+    return(OUT)
   }
   if(model == "Li" & align == TRUE){
     doMC::registerDoMC(threads)
@@ -51,6 +70,8 @@ dnastring2kaks <- function(cds,
       }
     }
     OUT <- as.data.frame(OUT)
+    attr(OUT, "model") <- "Li"
+    attr(OUT, "align") <- "TRUE"
     return(OUT)
   }
   if(model == "NG86" & align == FALSE){
@@ -64,6 +85,8 @@ dnastring2kaks <- function(cds,
       }
     }
     OUT <- as.data.frame(OUT)
+    attr(OUT, "model") <- "NG86"
+    attr(OUT, "align") <- "FALSE"
     return(OUT)
   }
   if(model == "NG86" & align == TRUE){
@@ -78,6 +101,8 @@ dnastring2kaks <- function(cds,
       }
     }
     OUT <- as.data.frame(OUT)
+    attr(OUT, "model") <- "NG86"
+    attr(OUT, "align") <- "TRUE"
     return(OUT)
   }
 }
